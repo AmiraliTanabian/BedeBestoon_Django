30ea1859c849241329dcd44f8167d458b0b62f1a
Amirali <atanabain@gmail.com>
2025-03-05 19:01:49 +0330

    Add Income, Spends, Token on the models

--- Spends/models.py
+++ Spends/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class Token(models.Model):
+    token = models.CharField(max_length=50)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+class Spend(models.Model):
+    title = models.CharField(max_length=255)
+    time = models.DateTimeField(auto_now_add=True)
+    price = models.BigIntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.title} - {self.time.strftime("%Y-%m-%d | %H:%M")}'
+
+class Income(models.Model):
+    title = models.CharField(max_length=255)
+    time = models.DateTimeField(auto_now_add=True)
+    price = models.BigIntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 
