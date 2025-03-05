93a9707bd323fb36fbd345fbb624b0f58aea25df
Amirali <atanabain@gmail.com>
2025-03-05 19:16:05 +0330

    add __str__ for models

--- Spends/models.py
+++ Spends/models.py
@@ -5,6 +5,9 @@ class Token(models.Model):
     token = models.CharField(max_length=50)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f'{self.user.first_name} --> {self.token}'
+
 class Spend(models.Model):
     title = models.CharField(max_length=255)
     time = models.DateTimeField()
@@ -19,5 +22,8 @@ class Income(models.Model):
     time = models.DateTimeField()
     price = models.BigIntegerField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.title} - {self.time.strftime("%Y-%m-%d | %H:%M")}'
 
 
