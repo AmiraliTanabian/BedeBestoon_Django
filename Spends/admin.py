059263adbc5ff6e3701ceeeee896b4601c618b5c
Amirali <atanabain@gmail.com>
2025-03-07 16:18:05 +0330

    Persianization and Personalization admin panel

--- Spends/admin.py
+++ Spends/admin.py
@@ -1,7 +1,21 @@
 from django.contrib import admin
 from . import models
+from django.contrib.auth.models import User
 
-admin.site.register(models.Income)
-admin.site.register(models.Spend)
+class AdminTempUser(admin.ModelAdmin):
+    readonly_fields = ['random_str']
+    list_display = ['__str__', 'date', 'email']
+    list_filter = ['date']
+
+
+class AdminSpendAndIncome(admin.ModelAdmin):
+    list_display =  ['title', 'price', 'time', 'user']
+    list_filter = ['time', 'user']
+
+
+class AdminUser(admin.ModelAdmin):
+    list_display = ['__str__', 'email', '']
+admin.site.register(models.Income, AdminSpendAndIncome)
+admin.site.register(models.Spend, AdminSpendAndIncome)
 admin.site.register(models.Token)
-admin.site.register(models.TempUser)
+admin.site.register(models.TempUser, AdminTempUser)
--- Spends/apps.py
+++ Spends/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class SpendsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'Spends'
+    verbose_name = 'ماژول اصلی'
