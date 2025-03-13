dbb3326f03d9a7af14f2d40ec892999fba8ae6f0
Amirali <atanabain@gmail.com>
2025-03-13 20:32:52 +0330

    create number validator for price field on add income or spend

--- Spends/forms.py
+++ Spends/forms.py
@@ -20,7 +20,8 @@ class loginForm(forms.Form):
 
 class addSpend(forms.Form):
     title = forms.CharField(label="عنوان خرج", widget=forms.TextInput(attrs = {'placeholder':'عنوان خرج'}))
-    price = forms.IntegerField(label="مبلغ خرج", widget=forms.TextInput(attrs={'placeholder':'مبلغ خرج'}))
+    price = forms.IntegerField(label="مبلغ خرج", widget=forms.NumberInput(attrs=
+                                                                          {"placeholder":"مبلغ"}))
     datetime = forms.DateTimeField(label="تاریخ و زمان خرج" ,widget=forms.DateTimeInput(attrs={'type':'datetime-local'}))
     is_now = forms.BooleanField(required=False, label="زمان الان را برای خرج ثبت کن", widget=forms.CheckboxInput())
     note = forms.CharField(label="یاداشت", widget=forms.Textarea(attrs={"placeholder":"یاداشت"}),
@@ -28,7 +29,7 @@ class addSpend(forms.Form):
 
 class addIncome(forms.Form):
     title = forms.CharField(label="عنوان درآمد", widget=forms.TextInput(attrs={'placeholder': 'عنوان درآمد'}))
-    price = forms.IntegerField(label="مبلغ درآمد", widget=forms.TextInput(attrs={'placeholder': 'مقدار  درآمد'}))
+    price = forms.IntegerField(label="مبلغ درآمد", widget=forms.NumberInput(attrs={'placeholder': 'مقدار  درآمد'}))
     datetime = forms.DateTimeField(label="تاریخ و زمان درآمد",
                                    widget=forms.DateTimeInput(attrs={'type': 'datetime-local'}))
     is_now = forms.BooleanField(required=False, label="زمان الان را برای درآمد ثبت کن", widget=forms.CheckboxInput())
