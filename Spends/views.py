b99986778bf85bc6c2cd189fb22fbd8e0126b538
Amirali <atanabain@gmail.com>
2025-03-19 19:05:37 +0330

    Remove old register view (FBV)

--- Spends/views.py
+++ Spends/views.py
@@ -41,63 +41,6 @@ def random_str(length):
     result = ''.join(choice(all_letters) for _ in range(length))
     return result
 
-def account_register(request):
-    if not request.user.is_authenticated:
-        if request.method != 'POST':
-            form_object = registerForm()
-            return render(request, 'Spends/register.html',
-                          {'status':False , 'form_object':form_object})
-
-        else:
-            form_object = registerForm(data=request.POST)
-            username = request.POST['username']
-            password = request.POST['password']
-            email = request.POST['email']
-            agian_password = request.POST['again_password']
-
-            if agian_password != password:
-                return render(request, 'Spends/register.html',
-                              {'status':'AgianPasswordError', 'form_object':form_object})
-
-            elif User.objects.filter(username=username).exists():
-                print('Username exists!')
-                return render(request, 'Spends/register.html',
-                              {'status':'UserNameExists', 'form_object':form_object})
-
-
-            elif User.objects.filter(email=email).exists():
-                print('Email Exists exists!')
-                return render(request, 'Spends/register.html',
-                              {'status':'EmailExists', 'form_object':form_object})
-
-            elif form_object.is_valid():
-                #TODO; check the user dont on temp users
-                #TODO; check ip
-                #TODO; remove TempUser
-                random_string = random_str(50)
-
-                # Add temp info on table
-                TempUser.objects.create(username=username, password=make_password(password),
-                                        date=timezone.now() , email=email, random_str=random_string)
-                # Send verify email
-                verification_url = settings.SITE_URL +  reverse('verify_account', args=[random_string])
-                email_subject = 'تایید حساب کاربری'
-                email_html_content = render_to_string('Spends/email_sender.html',
-                                                      {'verification_url':verification_url})
-                email_from = 'atanabain@gmail.com'
-
-                mail = EmailMessage(email_subject, email_html_content, email_from, [email])
-                mail.content_subtype = 'html'
-                mail.send()
-
-
-                return render(request, 'Spends/register.html',
-                              {'status':'OK', 'form_object':form_object})
-            else:
-                return render(request, 'Spends/register.html', {'form_object':form_object})
-    else:
-        return render(request, "Spends/login_register_limit.html")
-
 class AccountRegisterView(View):
     def get(self, request):
         if request.user.is_authenticated:
@@ -143,7 +86,6 @@ class AccountRegisterView(View):
                         {'status':'OK', 'form_object':form_object})
 
         else:
-            print(form_object.non_field_errors)
             return render(request, 'Spends/register.html',
                 {'status':False , 'form_object':form_object})
 
