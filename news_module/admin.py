e779a460b66cb51fc1cceed7448bd6fb97128488
Amirali <atanabain@gmail.com>
2025-03-27 21:16:20 +0330

    Complete new_module (without image serving)

--- news_module/admin.py
+++ news_module/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from .models import NewsModel
 
+class NewsModelAdmin(admin.ModelAdmin):
+    list_display = ["title", "writer"]
+    list_editable = ["is_active"]
+    list_filter = ["is_active"]
+    prepopulated_fields = {
+        "slug" : ("title",)
+    }
+
 admin.site.register(NewsModel)
--- news_module/models.py
+++ news_module/models.py
@@ -1,13 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.text import slugify
+
 
 class NewsModel(models.Model):
     writer = models.ForeignKey(User, on_delete=models.PROTECT, verbose_name="نویسنده")
     title = models.CharField(max_length=100, verbose_name="تیتر خبر")
     banner = models.ImageField(upload_to="NewsBanner/", verbose_name="تصویر اصلی")
-    slug = models.SlugField(db_index=True, verbose_name="اسلاگ")
+    slug = models.SlugField(db_index=True, verbose_name="اسلاگ", blank=True, allow_unicode=True)
     text = models.TextField()
-    datetime = models.DateTimeField(auto_now=True, verbose_name="آخرین ویرایش"),
+    datetime = models.DateField(verbose_name="آخرین ویرایش"),
     short_info = models.CharField(max_length=255,default="", verbose_name="توضیحات کوتاه")
     is_active = models.BooleanField(default=True, verbose_name="فعال بودن خبر")
 
@@ -18,3 +20,8 @@ class NewsModel(models.Model):
     class Meta:
         verbose_name= "خبر"
         verbose_name_plural = "اخبار"
+
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
